c08e0982e178b9d767c7f3a026b0af7133ca58cb
Cesare Ferrari <cesare@maryandferrari.com>
2019-09-04 23:11:16 -0400

    add restricted middleware

--- api/routers/jokes-router.js
+++ api/routers/jokes-router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Jokes = require('../jokes/jokes-model.js');
+const restricted = require('../../auth/restricted-middleware.js');
 
-// GET api/jokes/
-router.get('/', async (req, res) => {
+// GET /api/jokes/
+router.get('/', restricted, async (req, res) => {
   try {
     const jokes = await Jokes.all();
 
@@ -16,6 +17,24 @@ router.get('/', async (req, res) => {
     res.status(500).json({message: "Error getting jokes"});
   }
 });
+
+// GET /api/jokes/public
+router.get('/public', async (req, res) => {
+  try {
+    const publicJokes = await Jokes.public();
+
+    if (publicJokes) {
+      res.status(200).json(jokes);
+    } else {
+      res.status(400).json({message: "No jokes found"});
+    }
+  } catch (err) {
+  }
+})
+
+// GET /api/jokes/id/private/
+
+
 
 module.exports = router;
 
