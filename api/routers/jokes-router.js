ebf904b372ffc048361cb6dbb0bde3b281f8ccee
Cesare Ferrari <cesare@maryandferrari.com>
2019-09-10 22:59:23 -0400

    Add migrations

---
+++ api/routers/jokes-router.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const router = express.Router();
+const Jokes = require('../helpers/jokes-model.js')
+const Users = require('../helpers/users-model.js')
+const cors = require('cors')
+// const restricted = require('../helpers/restricted-middleware-jokes.js');
+
+router.use(cors())
+
+// GET /api/jokes/
+router.get('/', async (req, res) => {
+  const { id } = req.params
+  try {
+    const jokes = await Jokes.all();
+
+    if (jokes) {
+      res.status(200).json(jokes);
+    } else {
+      res.status(400).json({ message: "No jokes found" });
+    } 
+  } catch (err) {
+    res.status(500).json({message: "Error getting jokes"});
+  }
+});
+
+
+// GET /api/jokes/public
+router.get('/public', async (req, res) => {
+  try {
+    const publicJokes = await Jokes.public();
+    console.log(publicJokes);
+
+    if (publicJokes) {
+      res.status(200).json(publicJokes);
+    } else {
+      res.status(400).json({message: "No public jokes found"});
+    }
+  } catch (err) {
+  }
+})
+
+// specific joke comments
+router.get('/:id/comments', async (req, res) => {
+  const { id } = req.params
+  try {
+    const jokes = await Jokes.findComments(id)
+
+    if (jokes.length) {
+      res.json(jokes)
+    } else {
+      res.status(404).json({ message: 'Could not find jokes for given scheme' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get jokes' })
+  }
+})
+
+
+// GET by ID - /api/jokes/:id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const joke = await Jokes.findById(id)
+    if (joke) {
+      res.json(joke)
+    } else {
+      res.status(404).json({
+        message: 'Could not find joke with given id 🤷‍'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Failed to get joke ☠️'
+    })
+  }
+})
+
+// POST /api/jokes/
+router.post('/', async (req, res) => {
+  const joke = req.body;
+
+  try {
+    if (joke) {
+      const username = req.decodedJwt.username;
+      const [ user ] = await Users.findBy({username});
+      joke.user_id = user.id;
+
+      const [ jokeId ] = await Jokes.create(joke);
+      res.status(201).json(jokeId);
+    } else {
+      res.status(400).json({message: 'Joke required'});
+    }
+  } catch (err) {
+    res.status(500).json({message: 'Error adding joke'});
+  }
+});
+
+// DELETE /api/jokes/1
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleted = await Jokes.remove(id)
+    if (deleted) {
+      res.status(200).json(deleted);
+    } else {
+      res.status(404).json({message: 'Not found'});
+    }
+  } catch (err) {
+    res.status(500).json({message: 'Error deleting a joke'});
+  }
+})
+
+// PUT /api/jokes/1
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const jokeChanges = req.body;
+
+  try {
+    const joke = await Jokes.find(id);
+
+    if (joke) {
+      const updatedJoke = await Jokes.update(jokeChanges, id);
+      res.status(200).json(updatedJoke);
+    } else {
+      res.status(404).json({message: 'Joke not found'});
+    }
+  } catch (err) {
+    res.status(404).json({message: 'Error updating joke'});
+  }
+});
+
+
+module.exports = router;
---
+++ api/routers/users-router.js
@@ -0,0 +1,140 @@
+// Enable tools 🔨
+const router = require('express').Router()
+const Users = require('../helpers/users-model.js')
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+// const restricted = require('../restrictedMiddleware/users.js')
+
+router.use(cors())
+
+// Set up endpoints 💀
+router.get('/', async (req, res) => {
+  try {
+    const users = await Users.find()
+    if (users) {
+      res.status(200).json(users)
+    } else {
+      res.status(404).json({ message: 'Could not find users 🤷‍' })
+    }
+  } catch (e) {
+    res.status(500).json({ message: 'Failed to get users ☠️' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await Users.findById(id)
+    if (user) {
+      res.json(user)
+    } else {
+      res.status(404).json({
+        message: 'Could not find user with given id 🤷‍'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'Failed to get schemes ☠️'
+    })
+  }
+})
+
+router.get('/:id/jokes', async (req, res) => {
+  const { id } = req.params
+  try {
+    const jokes = await Users.findJokes(id)
+
+    if (jokes.length) {
+      res.json(jokes)
+    } else {
+      res.status(404).json({ message: 'Could not find jokes for given scheme' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get jokes' })
+  }
+})
+
+router.get('/:id/jokes/:id', async (req, res) => {
+  const { id } = req.params
+  console.log(req.params)
+  try {
+    const jokes = await Jokes.findById(id)
+    // const jokes = await Users.findJokes(id)
+
+    if (jokes.length) {
+      res.json(jokes)
+    } else {
+      res.status(404).json({ message: 'Could not find jokes with given id' })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get jokes' })
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params
+  const changes = {
+    id: req.body.id,
+    username: req.body.username,
+    email: req.body.email,
+    img_url: req.body.img_url,
+    password: req.body.password,
+
+  }
+  const token = genToken(changes)
+  try {
+    const user = await Users.findById(id)
+    console.log(`changes: ${changes}`)
+    console.log(`Token: ${req.params.token}`)
+    console.log(`id: ${id}`)
+    if (user) {
+      const updatedUser = await Users.update(changes, id)
+      res.status(200).json({ updatedUser, token })
+    } else {
+      res.status(404).json({
+        message: 'Could not find user with given id 🤷'
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Failed to update user ☠️' })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const deleted = await Users.remove(id)
+    if (deleted) {
+      res.json({ removed: deleted })
+    } else {
+      res.status(404).json({
+        message: 'Could not find the user with given id 🤷‍'
+      })
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to delete user ☠️' })
+  }
+})
+
+// Generate a JSON web token 🌹
+function genToken(user) {
+  const payload = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    img_url: user.img_url,
+    password: user.password
+  }
+
+  const secret = process.env.SECRET
+
+  const options = {
+    expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
+
+// Export router 🚀
+module.exports = router
